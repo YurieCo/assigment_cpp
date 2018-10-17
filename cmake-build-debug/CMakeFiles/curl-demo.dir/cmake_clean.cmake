40bf5789edc9c8f1a987a8e33ed39b4473f3b39b
pgi <cojocari.iurie@gmail.com>
2018-10-18 00:15:04 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/curl-demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/curl-demo.dir/main.cpp.o"
+  "curl-demo.pdb"
+  "curl-demo.exe"
+  "libcurl-demo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/curl-demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
